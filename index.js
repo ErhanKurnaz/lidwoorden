3b18cb90ad7a885126bcdcad446dc6fbd84c5b2e
Erhan Kurnaz <erhan@globalscreen.nl>
2020-06-30 09:41:17 +0200

    small improvement

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const { getHtml, getDeOfHet } = require('./lib/scraper')
 
 async function go() {
     const html = await getHtml(`https://www.welklidwoord.nl/${process.argv[2] || ''}`)
-    console.log(await getDeOfHet(html))
+    console.log((await getDeOfHet(html)) + ' ' + process.argv[2])
 }
 
 go()
